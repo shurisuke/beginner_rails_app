e04a3d716f284e8b8ae4bd213842f8f7b690d7b1
shuri <inoseshuri@icloud.com>
2025-09-05 14:17:27 +0900

    課題04実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_index_title, only: :index
+  before_action :set_show_title, only: :show
 
   # GET /users or /users.json
   def index
@@ -66,5 +68,13 @@ class UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :age)
+    end
+
+    def set_index_title
+      @index_title = 'ユーザー一覧'
+    end
+
+    def set_show_title
+      @show_title = 'ユーザー詳細'
     end
 end
